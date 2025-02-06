a38f1dfbaa65e1940c1863ed6d88b3b3ec92bc95
IshitaGH <ishitagh16@gmail.com>
2025-02-06 15:34:20 -0800

    Set up db and added rooommate and expenses tables

---
+++ backend/models/roommate.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from database import db
+
+
+class Room(db.Model):
+    __tablename__ = "rooms"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, unique=False, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(
+        DateTime(timezone=True),
+        default=datetime.now,
+        onupdate=datetime.now,
+        nullable=False,
+    )
+    invite_code = Column(String, nullable=False, unique=True)
+
+
+class Roommate(db.Model):
+    __tablename__ = "roommates"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, unique=False, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(
+        DateTime(timezone=True),
+        default=datetime.now,
+        onupdate=datetime.now,
+        nullable=False,
+    )
+    room_fkey = Column(Integer, ForeignKey("rooms.id"), nullable=True)
+    # TODO: password auth
+
+    expense_list = relationship(
+        "expenses", secondary="roommate_expenses", back_populates="roommate_list"
+    )
