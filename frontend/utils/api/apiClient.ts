01fa659ac4392f12003bb5fbe20377c019a0db1b
Byron Karlen <byronkarlen@gmail.com>
2025-02-13 13:43:07 -0800

    frontend config changes for backend API URL

--- frontend/utils/api/apiClient.ts
+++ frontend/utils/api/apiClient.ts
@@ -1,6 +1,5 @@
 // localhost doesn't work because Flask does not by default allow localhost in CORS policy (even though it resolves to 127.0.0.1)
-// const API_URL = "http://127.0.0.1:5000";
-const API_URL = "http://192.168.1.135:5000";  // set to your computer's IP if trying to use Expo Go
+const API_URL = process.env.API_URL || "http://127.0.0.1:5000";
 
 // returns jwt or throws an error
 export async function apiSignIn(username: string, password: string) {
