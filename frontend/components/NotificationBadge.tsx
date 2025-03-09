d585980d67504c9030e432b4e4cae3dd100ca799
clairebrenton <clairebrenton01@gmail.com>
2025-03-09 16:07:00 -0700

    Badge styling

--- frontend/components/NotificationBadge.tsx
+++ frontend/components/NotificationBadge.tsx
@@ -23,3 +23,30 @@ export const NotificationBadge: React.FC<NotificationBadgeProps> = ({ count, sho
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    badge: {
+      position: 'absolute',
+      right: -6,
+      top: -3,
+      backgroundColor: '#2196F3', // Blue color
+      borderRadius: 10,
+      minWidth: 20,
+      height: 20,
+      justifyContent: 'center',
+      alignItems: 'center',
+      zIndex: 10,
+    },
+    badgeText: {
+      color: 'white',
+      fontSize: 12,
+      fontWeight: 'bold',
+      paddingHorizontal: 4,
+    },
+    dot: {
+      width: 10,
+      height: 10,
+      borderRadius: 5,
+      backgroundColor: '#2196F3', // Blue color
+    },
+  });
