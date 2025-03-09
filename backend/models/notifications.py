ca61ab9503cb736d01c5ecfe5113916bcb3f22f3
clairebrenton <clairebrenton01@gmail.com>
2025-03-09 16:32:21 -0700

    Add is_read field to notifs model

--- backend/models/notifications.py
+++ backend/models/notifications.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from database import db
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 
 # from models.chore import Chore
@@ -17,3 +17,4 @@ class Notification(db.Model):
     notification_sender = Column(Integer, ForeignKey("roommates.id"), nullable=True)
     notification_recipient = Column(Integer, ForeignKey("roommates.id"), nullable=True)
     room_fkey = Column(Integer, ForeignKey("rooms.id"), nullable=True)
+    is_read = Column(Boolean, default=False, nullable=False)
